7f3e969b4b996c91d0878da30f86560ff6607ca2
Christopher McGowan <chris@chrisdba.com>
2021-10-24 21:53:42 -0500

    Update 000_grant_setup_users.sql
    Provide an example to make things usable.

--- 000_grant_setup_users.sql
+++ 000_grant_setup_users.sql
@@ -22,3 +22,8 @@ CREATE USER IF NOT EXISTS 'n_util' ACCOUNT LOCK;
 CREATE USER IF NOT EXISTS 'n_util_build' ACCOUNT LOCK;
 GRANT ALL ON `n_util_i`.* TO 'n_util_build' WITH GRANT OPTION;
 GRANT ALL ON `n_util`.* TO 'n_util_build' WITH GRANT OPTION;
+
+#--Grant your application and other users read/execute access to the public n_util schema
+	#--We recommend reviewing the available artifacts in the n_util schema in order to determine which users should have access
+    #--Artifacts in this schema are intended to be safer to allow access to than artifacts in the _i schema
+#--GRANT SELECT, EXECUTE ON `n_util`.* TO 'app_user';
