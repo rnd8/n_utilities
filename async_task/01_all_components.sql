b9b5460bafaa521bd7d9d5be034074a84b4fa58b
Christopher McGowan <chris@chrisdba.com>
2022-01-06 03:08:42 -0600

    Let's not forget the license stub.

--- async_task/01_all_components.sql
+++ async_task/01_all_components.sql
@@ -1,3 +1,22 @@
+/*
+    Copyright 2022 Christopher McGowan
+
+    This file is part of N_utilities.
+
+    N_utilities is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    N_utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with N_utilities.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 DROP TABLE IF EXISTS `n_util_i`.`async_task`;
 CREATE TABLE `n_util_i`.`async_task` (
   `task_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
--- crawler/05-crawler_job_registration_tools.sql
+++ crawler/05-crawler_job_registration_tools.sql
@@ -1,3 +1,22 @@
+/*
+    Copyright 2022 Christopher McGowan
+
+    This file is part of N_utilities.
+
+    N_utilities is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    N_utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with N_utilities.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 DELIMITER $$
 DROP PROCEDURE IF EXISTS `n_util`.`async_run_crawler_job`$$
 CREATE DEFINER=`n_util_build` PROCEDURE `n_util`.`async_run_crawler_job`(
